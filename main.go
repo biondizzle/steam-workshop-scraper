8f5afa0608c4b2c3df7ca598b671c60842e67fa3
biondizzle <biondizzle@gmail.com>
2022-10-29 04:01:48 -0400

    new new  url

--- main.go
+++ main.go
@@ -6,6 +6,6 @@ import (
 
 func main() {
 
-	workshop_scraper.Scrape("https://steamcommunity.com/workshop/browse/?appid=1435790&browsesort=trend&section=readytouseitems&requiredtags=2+or+more+players&created_date_range_filter_start=0&created_date_range_filter_end=0&updated_date_range_filter_start=0&updated_date_range_filter_end=0&actualsort=trend&p=10&days=-1")
+	workshop_scraper.Scrape("https://steamcommunity.com/workshop/browse/?appid=1435790&browsesort=trend&section=readytouseitems&requiredtags=2+or+more+players&created_date_range_filter_start=0&created_date_range_filter_end=0&updated_date_range_filter_start=0&updated_date_range_filter_end=0&actualsort=trend&p=1&days=-1")
 
 }
